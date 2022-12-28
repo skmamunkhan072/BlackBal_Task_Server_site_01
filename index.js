4449edac24dd1f759d9ce7bc4d63902876aec6a2
Sk Mamun Khan <skmamunkhan072@gmail.com>
2022-12-29 05:54:10 +0600

    mngodb conation conact

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.DATABASE_URL;
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 app.get("/", (req, res) => {
   res.send("my task server is running");
 });
-
 app.listen(port, () => {
   console.log("My Task server is running");
 });
